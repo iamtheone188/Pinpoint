d77f68caf7750da4f2fa543aaded9119d704257a
Kevin Han <kevinhan188@gmail.com>
2015-05-06 03:55:58 -0700

    Updated Stats.js

--- js/stats.js
+++ js/stats.js
@@ -1,20 +1,24 @@
 $(document).ready(function(){
     var username = getUrlParameter('username');
     
-    //Get JSON file and parse
-    var jsonData
-    $.ajax({
-        url: "../db/db.json",
-        //force to handle it as text
-        dataType: "text",
-        success: function(data) {
-            
-            //data downloaded so we call parseJSON function 
-            //and pass downloaded data
-            jsonData = $.parseJSON(data);
+    //Find username in stats
+    var info;
+    for(var i=0; i<stats.length; i++) {
+        if(stats[i].userName == username) {
+            info = stats[i];
+            break;
         }
-    });
-    console.log(jsonData.stats);
+    }
+    
+    var HTMLCode = "<table id=\"tableData\" class=\"table table-bordered\"><tbody>" +
+                   "<tr><td style=\"font-weight:bold\">Name</td><td>"+info.fullName+"</td></tr>" +
+                   "<tr><td style=\"font-weight:bold\">Total Number of Trips</td><td>"+info.numTrips+"</td></tr>" +
+                   "<tr><td style=\"font-weight:bold\">Number of Trips On Time</td><td>"+info.onTimeTrips+"</td></tr>" +
+                   "<tr><td style=\"font-weight:bold\">On-Time Percentage</td><td>"+info.onTimePct+"%</td></tr>" +
+                   "<tr><td style=\"font-weight:bold\">Total Omlet Dollars Earned</td><td>"+info.OMDEarned+" OMD</td></tr>" +
+                   "</tbody></table>";
+    $('#tableData').html(''); //Clear
+    $('#tableData').html(HTMLCode);
 });
 
 //Code taken from http://stackoverflow.com/questions/19491336/get-url-parameter-jquery
@@ -31,3 +35,47 @@ function getUrlParameter(sParam)
         }
     }
 }
+
+//Temporary Database
+var stats = [
+    {
+        "userName": "khan",
+        "fullName": "Kevin Han",
+        "numTrips": 50,
+        "onTimeTrips": 39,
+        "onTimePct": 78.0,
+        "OMDEarned": 3900
+    },
+    {
+        "userName": "gkho",
+        "fullName": "Gabriel Kho",
+        "numTrips": 40,
+        "onTimeTrips": 35,
+        "onTimePct": 87.5,
+        "OMDEarned": 3500
+    },
+    {
+        "userName": "jhdoe",
+        "fullName": "John Doe",
+        "numTrips": 36,
+        "onTimeTrips": 27,
+        "onTimePct": 75.0,
+        "OMDEarned": 2700
+    },
+    {
+        "userName": "jndoe",
+        "fullName": "Jane Doe",
+        "numTrips": 38,
+        "onTimeTrips": 36,
+        "onTimePct": 94.7,
+        "OMDEarned": 3600
+    },
+    {
+        "userName": "jsmith",
+        "fullName": "John Smith",
+        "numTrips": 150,
+        "onTimeTrips": 10,
+        "onTimePct": 6.7,
+        "OMDEarned": 1000
+    }
+]
